2e61486beb3de2184d8794a980bb554f62a9e3f9
Vishal Dharmawat <vishal.dharmawat@gmail.com>
2018-07-10 20:16:00 +0530

    withdraw Function implemented

---
+++ contracts/NatanToken.sol
@@ -0,0 +1,86 @@
+pragma solidity ^0.4.23;
+
+import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "./NatanConstant.sol";
+
+
+contract natanEduToken is  natanEduConstant, MintableToken {
+
+    /**
+     * @dev Pause token transfer. After successfully finished crowdsale it becomes true.
+     */
+    bool public paused = false;
+
+    uint _count;
+    /**
+     * @dev Accounts who can transfer token even if paused. Works only during crowdsale.
+     */
+    mapping(address => bool) excluded;
+
+    
+    function name() constant public returns (string _name) {
+        return TOKEN_NAME;
+    }
+
+    function symbol() constant public returns (string _symbol) {
+        return TOKEN_SYMBOL;
+    }
+
+    function decimals() constant public returns (uint _decimals) {
+        return TOKEN_DECIMALS;
+    }
+
+    function crowdsaleFinish() onlyOwner {
+        paused = true;
+        finishMinting();
+    }
+
+    function addExcluded(address _toExclude) onlyOwner {
+        excluded[_toExclude] = true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(!paused || excluded[_from] );
+        require(_from != COLD_WALLET && _to != COLD_WALLET);
+        return super.transferFrom(_from, _to, _value);
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(!paused || excluded[msg.sender]);
+        require(_to != COLD_WALLET);
+        return super.transfer(_to, _value);
+    }
+
+     /**
+   * @dev Function to mint tokens
+   */
+    function mint(address _to, uint256 _amount) public returns (bool)
+    {
+        super.mint(_to, _amount);
+    }
+
+    // This function will be used to withdraw 1/3  of the remaining token per year
+    function withdrawFromStorage(uint count) external  returns(bool) {
+        require(msg.sender == this);
+        _count = count;
+        uint value = withdrawAmount(_count);
+        balances[COLD_WALLET] = balances[COLD_WALLET].sub(value);
+        balances[NATANEDU_ADDRESS] = balances[NATANEDU_ADDRESS].add(value);
+        // allowed[COLD_WALLET][NATANEDU_ADDRESS] = allowed[COLD_WALLET][NATANEDU_ADDRESS].sub(value);
+        return true;
+    }
+    // This function will calculate the amount of token to withdraw from cold wallet each year
+    function withdrawAmount(uint count) internal returns(uint) {
+
+        if(count == 1){
+            return balanceOf(COLD_WALLET).div(3);
+        }
+        else if(count == 2){
+            return balanceOf(COLD_WALLET).div(2);
+        }
+        else if(count == 3){
+            return balanceOf(COLD_WALLET);
+        }
+        else revert();
+    }
+}

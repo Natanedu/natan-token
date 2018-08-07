f2fc97a4acf2b2e0bab8da3e7be5de64865bc1d3
Haythem Sellami <sellamihaythem96@gmail.com>
2018-08-07 21:49:21 +0100

    redo NatanSale unit tests

--- test/TestNatanSale.js
+++ test/TestNatanSale.js
@@ -56,80 +56,63 @@ contract('NatanSale', function(accounts) {
   let natanSale;
   let natanToken;
   let natanTokenAddress;
-  let rate = 0;
 
   before(async() => {
     owner = accounts[0];
-    wallet = accounts[1];
-    natanSale = await NatanSale.new(openingTime.getTime(), endingTime.getTime(), wallet, {from: owner});
+    natanSale = await NatanSale.new(openingTime.getTime(), endingTime.getTime(), {from: owner});
     natanTokenAddress = await natanSale.token.call();
     natanToken = await NatanToken.at(natanTokenAddress, {from: owner});
   });
 
-  describe("Token Sale Rate", async () => {
-
-    it("Rate", async function () {
-      natanSale.getRate().then(res => {
-        rate = res.toNumber();
-        let now = new Date().getTime();
-        let date = new Date(openingTime);
-        //console.log("       Rate   : " + rate);
-        //console.log("       Now    : " + now);
-        //console.log("       Date   : " + date.getTime());
-        if(now <= date.setDate(openingTime.getDate() + 4)) {
-          assert.equal(rate,10000*DECIMALSFACTOR);
-        }
-        else if((now >= date.setDate(openingTime.getDate() + 7)) && (now <= date.setDate(openingTime.getDate() + 11))) {
-          assert.equal(rate,5000*DECIMALSFACTOR);
-        }
-        else if((now >= date.setDate(openingTime.getDate() + 14)) && (now <= date.setDate(openingTime.getDate() + 17))) {
-          assert.equal(rate,3000*DECIMALSFACTOR);
-        }
-        else if((now >= date.setDate(openingTime.getDate() + 32)) && (now <= date.setDate(openingTime.getDate() + 35))) {
-          assert.equal(rate,1000*DECIMALSFACTOR);
-        }
-        else {
-          assert.equal(rate,500*DECIMALSFACTOR);
-        }
-      });
-    });
-
-  });
-
   describe("Buy tokens", async () => {
     let beneficiary;
-    const weiamount = 1;
-    let rate;
-    let tokens;
+    const dollaramount = 150;
+    let tokensAmount = 0;
+    let rate = 0;
 
     before(async () => {
       beneficiary = accounts[3];
-      natanSale.getRate().then(async (res) => {
-        rate = res.toNumber();
-        tokens = weiamount*rate;
-
-        console.log("       Rate   : " + rate);
-        console.log("       tokens = " + tokens);
-        console.log("       mini   = " + MINIMAL_PURCHASE);
-        console.log("       maxi   = " + MAXIMUM_PURCHASE);
-      });
+      console.log("       mini          = " + MINIMAL_PURCHASE);
+      console.log("       max           = " + MAXIMUM_PURCHASE);
     });
 
     it("buy tokens", async () => {
-      //let tokenAmount = await natanSale._getTokenAmount(weiamount, {from: beneficiary});
-      //console.log("       Amount = " + tokenAmount + "( using _getTokenAmount() )");
-      //console.log(tokenAmount >= MINIMAL_PURCHASE);
-      //console.log(tokenAmount <= MAXIMUM_PURCHASE);
-      await natanSale.buyTokens(beneficiary, weiamount, {from: beneficiary});
+      let now = new Date().getTime();
+      let date = new Date(openingTime);
+
+      if(now <= date.setDate(openingTime.getDate() + 4)) {
+        rate = 6;
+      }
+      else if((now > date.setDate(openingTime.getDate() + 6)) && (now <= date.setDate(openingTime.getDate() + 11))){
+        rate = 7;
+      }
+      else if ((now > date.setDate(openingTime.getDate() + 14)) && (now <= date.setDate(openingTime.getDate() + 18))){
+        rate = 8;
+      }
+      else {
+        rate = 10;
+      }
+
+      tokensAmount = (dollaramount * 10) / rate;
+      console.log("       rate          = " + rate);
+      console.log("       tokens amount = " + tokensAmount);
+
+      //change isValidPurchase() to public if you want to test this
+      /*natanSale.isValidPurchase(beneficiary, tokensAmount).then((res) => {
+        console.log(res);
+      });*/
+      
+      await natanSale.buyTokens(beneficiary, dollaramount, {from: owner});
+     
       natanToken.balanceOf(beneficiary).then((res) => {
         let balance = res.toNumber();
-        assert.equal(balance,tokens);
+        assert.equal(balance,tokensAmount);
       });
     });
 
     it("beneficiary should Fail to have balance more than maximum purchase", async () => {
       try {
-        await natanSale.buyTokens(beneficiary, weiamount, {from: beneficiary});
+        await natanSale.buyTokens(beneficiary, 5000000, {from: owner});
       } catch (error) {
           //logError(" Beneficiary with address 0x0 tried to buy tokens and failed");
           return true;
@@ -139,7 +122,7 @@ contract('NatanSale', function(accounts) {
 
     it("invalid beneficiary should Fail to buy tokens", async () => {
       try {
-        await natanSale.buyTokens(0, weiamount, {from: beneficiary});
+        await natanSale.buyTokens(0, dollaramount, {from: owner});
       } catch (error) {
           //logError(" Beneficiary with address 0x0 tried to buy tokens and failed");
           return true;
@@ -149,7 +132,7 @@ contract('NatanSale', function(accounts) {
     
     it("beneficiary should Fail to buy amout of tokens inferior to the MINIMAL_PURCHASE", async () => {
       try {
-        await natanSale.buyTokens(beneficiary, 0.001, {from: beneficiary});
+        await natanSale.buyTokens(beneficiary, 0.1, {from: owner});
       } catch (error) {
         //logError(" Beneficiary tried to buy amount tokens < minimal purchase");
         return true;
@@ -159,7 +142,7 @@ contract('NatanSale', function(accounts) {
 
     it("beneficiary should Fail to buy amout of tokens superior to the MAXIMUM_PURCHASE", async () => {
       try {
-        await natanSale.buyTokens(beneficiary, 1, {from: beneficiary});
+        await natanSale.buyTokens(beneficiary, 5000000, {from: owner});
       } catch (error) {
         //logError(" Beneficiary tried to buy amount tokens > minimal purchase");
         return true;
@@ -169,165 +152,103 @@ contract('NatanSale', function(accounts) {
  
   });
 
-  describe("Crowdsale", async () => {
+  describe("Pause/Resume sale", async() => {
 
-    it("should Fail to finish crowdsale before closing time", async () => {
+    it("should Fail to pause sale from unauthorized source", async () => {
       try {
-        await natanSale.finalize({from: owner});
+        await natanSale.pauseSale({from: accounts[3]});
       } catch (error) {
           //logError(" Tried to withdraw for the second time in the same year and failed");
           return true;
       }
       throw new Error("I should never see this!");
-    });    
+    });   
+
+    it("should Fail to resume unpaused sale from unauthorized source", async () => {
+      try {
+        await natanSale.resumeSale({from: owner});
+      } catch (error) {
+          //logError(" Tried to withdraw for the second time in the same year and failed");
+          return true;
+      }
+      throw new Error("I should never see this!");
+    });  
+
+    it("Pause sale", async() => {
+      await natanSale.pauseSale({from: owner});
+      natanSale.isPaused.call().then((res) => {
+        assert.equal(res, true);
+      });
+    });
+
+    it("should Fail to resume sale from unauthorized source", async () => {
+      try {
+        await natanSale.resumeSale({from: accounts[3]});
+      } catch (error) {
+          //logError(" Tried to withdraw for the second time in the same year and failed");
+          return true;
+      }
+      throw new Error("I should never see this!");
+    });  
+    
+    it("Resume sale", async() => {
+      await natanSale.resumeSale({from: owner});
+      natanSale.isPaused.call().then((res) => {
+        assert.equal(res, false);
+      });
+    });
 
   });
 
-  describe("Crowdsale", async () => {
+  describe("Finish sale", async () => {
+
+    it("should Fail to finish crowdsale before closing time", async () => {
+      try {
+        await natanSale.finalizeSale({from: owner});
+      } catch (error) {
+          //logError(" Tried to withdraw for the second time in the same year and failed");
+          return true;
+      }
+      throw new Error("I should never see this!");
+    });   
+    
+    it("should Fail to finish crowdsale from unauthorized source", async () => {
+      try {
+        await natanSale.finalizeSale({from: accounts[9]});
+      } catch (error) {
+          //logError(" Tried to withdraw for the second time in the same year and failed");
+          return true;
+      }
+      throw new Error("I should never see this!");
+    });   
+
+  });
+
+  describe("Finish sale", async () => {
 
     before(async () => {
       await timeTravel(86400 * 365); // Move forward a year in time
       await mineBlock();  // workaround for https://github.com/ethereumjs/testrpc/issues/336
     });
 
-    it("should Fail to finish crowdsale from unauthorized source", async () => {
-      try {
-        await natanSale.finalize({from: accounts[5]});
-      } catch (error) {
-          //logError(" Tried to withdraw for the second time in the same year and failed");
-          return true;
-      }
-      throw new Error("I should never see this!");
-    });
-
     it("finish crowdsale", async () => {
       let currentBlock = await web3.eth.getBlock("latest");
-      await natanSale.finalize({from: owner});
+      await natanSale.finalizeSale({from: owner});
       natanSale.isFinalized.call().then((res) => {
         assert.equal(res, true);
       });
     });
 
+    it("should Fail to finish crowdsale after it's already finished", async () => {
+      try {
+        await natanSale.finalizeSale({from:owner});
+      } catch (error) {
+          //logError(" Tried to withdraw for the second time in the same year and failed");
+          return true;
+      }
+      throw new Error("I should never see this!");
+    });   
+
   });
   
-  describe("Withdraw tokens for the first year", async () => {
-
-    it("should withdraw 1/3 of the remaining token for the first year", async () => {
-      let currentBlock = await web3.eth.getBlock("latest");
-      await natanSale.withdrawFromStorage({from: owner}); 
-      natanSale.count.call((res) => {
-        assert.equal(res, 1);
-      });     
-    });
-
-  });
-
-  describe("Try withdraw tokens second time for the first year", async () => {
-
-    before(async () => {
-      await timeTravel(86400 * 60); // Move forward 2 month in time
-      await mineBlock();  // workaround for https://github.com/ethereumjs/testrpc/issues/336
-    });
-
-    it("should FAIL to withdraw two times in the year 2019", async () => {
-      try {
-        await natanSale.withdrawFromStorage({from: owner});
-      } catch (error) {
-          //logError(" Tried to withdraw for the second time in the same year and failed");
-          return true;
-      }
-      throw new Error("I should never see this!");
-    });
-
-  });
-
-  describe("Withdraw tokens for the second year", async () => {
-
-    before(async () => {
-      await timeTravel(86400 * 305); // Move forward a year in time
-      await mineBlock()
-    });
-
-    it("should withdraw 2/3 of the remaining token for the year 2020", async () => {
-      await natanSale.withdrawFromStorage({from: owner});
-      natanSale.count.call((res) => {
-        assert.equal(res, 2);
-      });     
-    });
-
-  });
-
-  describe("Try to withdraw tokens second time for the second year", async () => {
-
-    before(async () => {
-      await timeTravel(86400 * 60); // Move forward 2 month in time
-      await mineBlock();  // workaround for https://github.com/ethereumjs/testrpc/issues/336
-    });
-
-    it("should FAIL to withdraw two times in the year 2020", async () => {
-      try {
-        await natanSale.withdrawFromStorage({from: owner});
-      } catch (error) {
-          //logError(" Tried to withdraw for the second time in the same year and failed");
-          return true;
-      }
-      throw new Error("I should never see this!");
-    });
-
-  });
-
-  describe("Withdraw tokens for the third year", async () => {
-
-    before(async () => {
-      await timeTravel(86400 * 305); // Move forward a year in time
-      await mineBlock()
-    });
-
-    it("should withdraw 3/3 of the remaining token for the year 2021", async () => {
-      let isDone = await natanSale.withdrawFromStorage({from: owner});
-      natanSale.count.call((res) => {
-        assert.equal(res, 3);
-      });     
-    });
-
-  });
-
-  describe("Try to withdraw tokens second time for the third year", async () => {
-
-    before(async () => {
-      await timeTravel(86400 * 60); // Move forward 2 month in time
-      await mineBlock();  // workaround for https://github.com/ethereumjs/testrpc/issues/336
-    });
-
-    it("should FAIL to withdraw two times in the year 2021", async () => {
-      try {
-        await natanSale.withdrawFromStorage({from: owner});
-      } catch (error) {
-          //logError(" Tried to withdraw for the second time in the same year and failed");
-          return true;
-      }
-      throw new Error("I should never see this!");
-    });
-
-  });
-
-  describe("Try to withdraw tokens for the forth year", async () => {
-
-    before(async () => {
-      await timeTravel(86400 * 305); // Move forward a year in time
-      await mineBlock()
-    });
-
-    it("should FAIL to withdraw in the year 2022", async () => {
-      try {
-        await natanSale.withdrawFromStorage({from: owner});
-      } catch (error) {
-          //logError(" Tried to withdraw for the second time in the same year and failed");
-          return true;
-      }
-      throw new Error("I should never see this!");
-    });
-
-  });
-
 });
